ea824ee7fdfb325412972b12509551be3662531f
LeonardoSecomandi <62284402+LeonardoSecomandi@users.noreply.github.com>
2022-03-08 22:40:04 +0100

    BookManager.API

---
+++ BookManager/BookManager/BookManager.API/Controllers/CategoryController.cs
@@ -0,0 +1,38 @@
+﻿using BookManager.API.Models.DTOS.Responses;
+using BookManager.API.Models.Interfaces;
+using BookManager.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookManager.API.Controllers
+{
+    [ApiController]
+    [Route("[Controller]/api")]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryInterface _categoryService;
+        public CategoryController(ICategoryInterface categorys)
+        {
+            this._categoryService = categorys;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<AddCategoryResponse>> AddCategory(Categories category)
+        {
+            try
+            {
+                var result = await _categoryService.AddCategory(category);
+                return Ok(result);
+            }
+            catch (Exception)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Errore nel recupero dati dal database");
+            }
+        }
+
+    }
+}

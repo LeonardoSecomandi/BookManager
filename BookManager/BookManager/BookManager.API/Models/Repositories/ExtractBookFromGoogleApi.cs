882e9014a3f1c65de823b697bd81e92a87d3a2b3
LeonardoSecomandi <62284402+LeonardoSecomandi@users.noreply.github.com>
2022-03-09 09:14:07 +0100

    BookManager.API

---
+++ BookManager/BookManager/BookManager.API/Models/Repositories/ExtractBookFromGoogleApi.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookManager.API.Data;
+
+namespace BookManager.API.Models.Repositories
+{
+    public class ExtractBookFromGoogleApi
+    {
+        private readonly ApplicationDbContext _context;
+        public ExtractBookFromGoogleApi(ApplicationDbContext context)
+        {
+            this._context = context;
+        }
+        
+        public void ExtractBook(string Link)
+        {
+
+        }
+    }
+}

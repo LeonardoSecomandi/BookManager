7481af5186fb7aa169b59c50daf4c17259011239
LeonardoSecomandi <62284402+LeonardoSecomandi@users.noreply.github.com>
2022-04-21 12:19:40 +0200

    BookManager

---
+++ BookManager/BookManager/BookManager.API/Models/DTOS/Responses/ItemResponse.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookManager.API;
+using BookManager.Models;
+
+namespace BookManager.API.Models.DTOS.Responses
+{
+    public class ItemResponse
+    {
+        public Book Book { get; set; }
+        public List<Review> ItemReviewList { get; set; }
+        public List<Discussion> ItemDiscussionList { get; set; }
+    }
+}
---
+++ BookManager/BookManager/BookManager.API/Models/Interfaces/IItemInterface.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookManager.API.Models.DTOS.Responses;
+using BookManager.Models;
+
+namespace BookManager.API.Models.Interfaces
+{
+    interface IItemInterface
+    {
+        public Task<IEnumerable<ItemResponse>> GetItems();
+    }
+}
